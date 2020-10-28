0cb0e164636f0eca3ebe9b2a9d5cc810a4fcc4a6
胡晟昊 <863116814@qq.com>
2020-10-28 14:38:16 +0800

    cat 图片分享

--- app/src/main/java/com/dede/android_eggs/SettingsActivity.kt
+++ app/src/main/java/com/dede/android_eggs/SettingsActivity.kt
@@ -1,13 +1,18 @@
 package com.dede.android_eggs
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.browser.trusted.TrustedWebActivityIntentBuilder
 import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.forEachIndexed
 import com.google.androidbrowserhelper.trusted.TwaLauncher
+import kotlinx.coroutines.GlobalScope
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -27,12 +32,17 @@ class SettingsActivity : AppCompatActivity() {
         companion object {
             const val KEY_GITHUB = "key_github"
             const val KEY_SOURCE = "key_source"
+            const val KEY_EGG_GROUP = "key_egg_group"
         }
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
             findPreference<Preference>(KEY_GITHUB)?.onPreferenceClickListener = this
             findPreference<Preference>(KEY_SOURCE)?.onPreferenceClickListener = this
+            findPreference<PreferenceCategory>(KEY_EGG_GROUP)?.forEachIndexed { _, preference ->
+                preference.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    .addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+            }
         }
 
         override fun onPreferenceClick(preference: Preference?): Boolean {
