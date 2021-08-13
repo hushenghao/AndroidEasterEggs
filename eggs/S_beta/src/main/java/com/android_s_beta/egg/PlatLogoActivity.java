e2eeb56a169dae0169ffab22c05e43985ee9c328
胡晟昊 <863116814@qq.com>
2021-08-13 18:09:37 +0800

    Android S beta4

---
+++ eggs/S_beta/src/main/java/com/android_s_beta/egg/PlatLogoActivity.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//package com.android.internal.app;
+package com.android_s_beta.egg;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Android 12 beta 4 Easter egg
+ */
+public class PlatLogoActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final float dp = getResources().getDisplayMetrics().density;
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(0);
+            getWindow().setStatusBarColor(0);
+        }
+
+        final ActionBar ab = getActionBar();
+        if (ab != null) ab.hide();
+
+    }
+
+    private void launchNextStage(boolean locked) {
+        //finish(); // no longer finish upon unlock; it's fun to frob the dial
+    }
+
+}
+
+
+
