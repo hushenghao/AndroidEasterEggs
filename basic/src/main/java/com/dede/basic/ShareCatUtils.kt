db47b562cd51a83c9f36c3576bdc81c1bbacbc0f
胡晟昊 <863116814@qq.com>
2021-10-21 18:02:52 +0800

    fix cat share

--- basic/src/main/java/com/dede/basic/ShareCatUtils.kt
+++ basic/src/main/java/com/dede/basic/ShareCatUtils.kt
@@ -7,22 +7,16 @@ import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.core.content.FileProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.OutputStream
 
 
 /**
@@ -33,74 +27,64 @@ import java.io.OutputStream
 object ShareCatUtils {
 
     private const val CATS_DIR = "Cats"
+    private const val MIME_TYPE = "image/png"
 
-    @RequiresApi(Build.VERSION_CODES.Q)
-    private fun saveCatAtQ(context: Context, bitmap: Bitmap, catName: String): Uri? {
-        val resolver = context.contentResolver
-        val imageCollection =
-            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-        val imageDetail = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, catName.toFileName())
-            put(MediaStore.Images.Media.RELATIVE_PATH, CATS_DIR)
-            put(MediaStore.Images.Media.IS_PENDING, 1)
+    private suspend fun saveCat(context: Context, bitmap: Bitmap, catName: String): Uri? =
+        withContext(Dispatchers.IO) {
+            // https://developer.android.google.cn/training/data-storage/shared/media
+            // 图片信息
+            val imageValues = ContentValues().apply {
+                put(MediaStore.Images.Media.DISPLAY_NAME, catName.toFileName())
+                put(MediaStore.Images.Media.MIME_TYPE, MIME_TYPE)
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                imageValues.put(
+                    MediaStore.Images.Media.RELATIVE_PATH,
+                    "${Environment.DIRECTORY_PICTURES}/$CATS_DIR"
+                )
+                imageValues.put(MediaStore.Images.Media.IS_PENDING, 1)
+            } else {
+                val dir = File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    CATS_DIR
+                )
+                if (!dir.exists() && !dir.mkdirs()) {
+                    Log.e("NekoLand", "save: error: can't create Pictures directory")
+                    return@withContext null
+                }
+                val png = File(dir, catName.toFileName())
+                Log.v("Neko", "cat file: $png")
+                imageValues.put(MediaStore.Images.Media.DATA, png.absolutePath)
+            }
+
+            // 保存的位置
+            val imageCollection =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+                } else {
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                }
+
+            val resolver = context.contentResolver
+
+            // 插入图片信息
+            val imageUri = resolver.insert(imageCollection, imageValues)
+            if (imageUri == null) {
+                Log.w("NekoLand", "insert: error: uri == null")
+                return@withContext null
+            }
+            Log.v("Neko", "cat uri: $imageUri")
+
+            // 保存图片
+            resolver.runCatching {
+                openOutputStream(imageUri, "w").use {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 0, it)
+                }
+            }.onFailure {
+                Log.e("NekoLand", "save: error: $it")
+            }
+            imageUri
         }
-
-        val imageContentUri = resolver.insert(imageCollection, imageDetail)
-        if (imageContentUri == null) {
-            Log.w("NekoLand", "insert: error: uri == null")
-            return null
-        }
-
-        try {
-            val os = resolver.openOutputStream(imageContentUri, "w")
-            bitmap.compress(Bitmap.CompressFormat.PNG, 0, os)
-        } catch (e: IOException) {
-            Log.e("NekoLand", "save: error: $e")
-        }
-
-        imageDetail.clear()
-        imageDetail.put(MediaStore.Images.Media.IS_PENDING, 0)
-        resolver.update(imageContentUri, imageDetail, null, null)
-        return imageContentUri
-    }
-
-    private fun saveCatPath(catName: String):String {
-        val dir = File(
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-            CATS_DIR
-        )
-        return File(dir, catName.toFileName()).absolutePath
-    }
-
-    private fun saveCat(context: Context, bitmap: Bitmap, catName: String): Uri? {
-        val dir = File(
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-            CATS_DIR
-        )
-        if (!dir.exists() && !dir.mkdirs()) {
-            Log.e("NekoLand", "save: error: can't create Pictures directory")
-            return null
-        }
-        val png = File(dir, catName.toFileName())
-        Log.v("Neko", "cat file: $png")
-        try {
-            val os: OutputStream = FileOutputStream(png)
-            bitmap.compress(Bitmap.CompressFormat.PNG, 0, os)
-            os.close()
-        } catch (e: IOException) {
-            Log.e("NekoLand", "save: error: $e")
-            return null
-        }
-
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            FileProvider.getUriForFile(
-                context,
-                context.packageName + ".fileprovider", png
-            )
-        } else {
-            Uri.fromFile(png)
-        }
-    }
 
     private fun String.toFileName(): String {
         return this.replace("[/ #:]+".toRegex(), "_") + ".png"
@@ -110,27 +94,13 @@ object ShareCatUtils {
     fun share(activity: Activity, bitmap: Bitmap, catName: String) {
 
         GlobalScope.launch {
-            val uri = withContext(Dispatchers.IO) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    saveCatAtQ(activity, bitmap, catName)
-                } else {
-                    saveCat(activity, bitmap, catName)
-                }
-            } ?: return@launch
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-                MediaScannerConnection.scanFile(
-                    activity,
-                    arrayOf(saveCatPath(catName)),
-                    arrayOf("image/png"),
-                    null
-                )
-            }
-            Log.v("Neko", "cat uri: $uri")
+            val uri = saveCat(activity, bitmap, catName) ?: return@launch
+
             val intent = Intent(Intent.ACTION_SEND)
             intent.putExtra(Intent.EXTRA_STREAM, uri)
             intent.putExtra(Intent.EXTRA_SUBJECT, catName)
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-            intent.type = "image/png"
+            intent.type = MIME_TYPE
             activity.startActivity(
                 Intent.createChooser(intent, null)
                     .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
