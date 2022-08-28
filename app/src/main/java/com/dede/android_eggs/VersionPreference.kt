1f0ed435d270c827e7e1160cc827644357c79982
胡晟昊 <863116814@qq.com>
2022-08-28 16:00:44 +0800

    feat: MD3

--- app/src/main/java/com/dede/android_eggs/VersionPreference.kt
+++ app/src/main/java/com/dede/android_eggs/VersionPreference.kt
@@ -1,8 +1,11 @@
 package com.dede.android_eggs
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.drawable.ScaleDrawable
 import android.util.AttributeSet
-import androidx.preference.Preference
+import android.view.Gravity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.dede.android_eggs.BuildConfig.VERSION_CODE
 import com.dede.android_eggs.BuildConfig.VERSION_NAME
 
@@ -19,5 +22,12 @@ class VersionPreference : ChromeTabPreference {
 
     init {
         summary = context.getString(R.string.summary_version, VERSION_NAME, VERSION_CODE)
+        val bitmap =
+            BitmapFactory.decodeResource(context.resources, R.drawable.ic_logo_color_pgyer)
+        val drawable = RoundedBitmapDrawableFactory.create(context.resources, bitmap)
+        drawable.isCircular = true
+        icon = ScaleDrawable(drawable, Gravity.CENTER, 1f, 1f).apply {
+            level = 8500
+        }
     }
 }
