dce1d6d1531b02d8069713b4ed12f5e16b14b6a3
Hu Shenghao <dede.hu@qq.com>
2023-01-17 18:02:14 +0800

    feat: Added Font Icons

--- app/src/main/java/com/dede/android_eggs/FontIconsDrawable.kt
+++ app/src/main/java/com/dede/android_eggs/FontIconsDrawable.kt
@@ -5,14 +5,17 @@ import android.content.res.ColorStateList
 import android.graphics.*
 import android.graphics.Paint.FontMetrics
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.text.TextPaint
-import android.util.Log
-import android.util.TypedValue
 import androidx.annotation.Dimension
+import androidx.core.graphics.component1
+import androidx.core.graphics.component2
+import androidx.core.graphics.component3
+import androidx.core.graphics.component4
+import com.dede.basic.dp
 import com.dede.basic.globalContext
 import com.google.android.material.color.MaterialColors
 import kotlin.math.min
-import kotlin.math.roundToInt
 
 /**
  * Material Icons.
@@ -34,6 +37,8 @@ class FontIconsDrawable(
 
     private val paint = TextPaint(Paint.ANTI_ALIAS_FLAG)
     private val metrics = FontMetrics()
+    private val padding = Rect()
+    private val tempBounds = Rect()
 
     private var dimension: Int = -1
     private var colorStateList: ColorStateList? = null
@@ -42,25 +47,21 @@ class FontIconsDrawable(
         paint.typeface = ICONS_TYPEFACE
         paint.textAlign = Paint.Align.CENTER
         val color = MaterialColors.getColor(
-            context,
-            com.google.android.material.R.attr.colorAccent,
-            Color.WHITE
+            context, com.google.android.material.R.attr.colorAccent, Color.WHITE
         )
         paint.color = color
         if (size > 0) {
-            dimension = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                size,
-                context.resources.displayMetrics
-            ).roundToInt()
+            dimension = size.dp
             paint.textSize = dimension.toFloat()
             paint.getFontMetrics(metrics)
         }
     }
 
     fun setColor(color: Int) {
-        paint.color = color
-        invalidateSelf()
+        if (color != paint.color) {
+            paint.color = color
+            invalidateSelf()
+        }
     }
 
     fun setColorStateList(colorStateList: ColorStateList?) {
@@ -85,16 +86,65 @@ class FontIconsDrawable(
         return if (dimension > 0) dimension else -1
     }
 
-    override fun onBoundsChange(bounds: Rect) {
+    override fun getPadding(padding: Rect): Boolean {
+        padding.set(this.padding)
+        return true
+    }
+
+    fun setPadding(@Dimension padding: Int) {
+        this.setPadding(padding, padding, padding, padding)
+    }
+
+    fun setPadding(
+        @Dimension left: Int,
+        @Dimension top: Int,
+        @Dimension right: Int,
+        @Dimension bottom: Int,
+    ) {
+        if (this.padding.left == left && this.padding.top == top &&
+            this.padding.right == right && this.padding.bottom == bottom
+        ) return
+
+        this.padding.set(left, top, right, bottom)
+        computeIconSize()
+        invalidateSelf()
+    }
+
+    fun setPadding(padding: Rect) {
+        setPadding(padding.left, padding.top, padding.right, padding.bottom)
+    }
+
+    private fun computeIconSize() {
         if (dimension > 0) {
-            return
+            tempBounds.set(0, 0, dimension, dimension)
+        } else {
+            tempBounds.set(bounds)
         }
 
-        val size = min(bounds.width(), bounds.height())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            tempBounds.inset(padding.left, padding.top, padding.right, padding.bottom)
+        } else {
+            val (left, top, right, bottom) = tempBounds
+            tempBounds.set(
+                left + padding.left,
+                top + padding.top,
+                right - padding.right,
+                bottom - padding.bottom
+            )
+        }
+
+        val size = min(tempBounds.width(), tempBounds.height())
         if (size <= 0) return
 
         paint.textSize = size.toFloat()
         paint.getFontMetrics(metrics)
+    }
+
+    override fun onBoundsChange(bounds: Rect) {
+        if (dimension > 0) {
+            return
+        }
+        computeIconSize()
     }
 
     override fun draw(canvas: Canvas) {
@@ -104,8 +154,9 @@ class FontIconsDrawable(
         if (colorStateList != null) {
             paint.color = colorStateList.getColorForState(state, colorStateList.defaultColor)
         }
-        val y = (metrics.descent - metrics.ascent) / 2 - metrics.ascent / 2
-        canvas.drawText(unicode, bounds.exactCenterX(), y, paint)
+        val y = (metrics.descent - metrics.ascent) / 2 - metrics.ascent / 2 + padding.top
+        val x = tempBounds.exactCenterX() + padding.left
+        canvas.drawText(unicode, x, y, paint)
     }
 
     override fun setAlpha(alpha: Int) {
@@ -114,8 +165,6 @@ class FontIconsDrawable(
     }
 
     override fun setColorFilter(colorFilter: ColorFilter?) {
-        paint.colorFilter = colorFilter
-        invalidateSelf()
     }
 
     override fun getOpacity(): Int {
--- app/src/main/java/com/dede/android_eggs/NavigationViewController.kt
+++ app/src/main/java/com/dede/android_eggs/NavigationViewController.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.content.res.Configuration
-import android.graphics.Color
 import android.net.Uri
 import android.view.Menu
 import android.view.MenuItem
@@ -15,10 +14,8 @@ import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.TintTypedArray
 import androidx.core.content.ContextCompat
-import androidx.core.content.withStyledAttributes
 import androidx.core.view.OnApplyWindowInsetsListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -26,13 +23,14 @@ import androidx.core.view.updatePadding
 import androidx.drawerlayout.widget.DrawerLayout
 import com.dede.android_eggs.databinding.ActivityEasterEggsBinding
 import com.dede.android_eggs.databinding.LayoutNavigationHeaderBinding
+import com.dede.basic.dp
 import com.dede.basic.getBoolean
 import com.dede.basic.putBoolean
 import com.dede.basic.string
-import com.google.android.material.color.MaterialColors
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.resources.MaterialAttributes
 import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.R as M3R
 
 class NavigationViewController(private val activity: AppCompatActivity) {
 
@@ -53,7 +51,7 @@ class NavigationViewController(private val activity: AppCompatActivity) {
             R.string.label_drawer_close
         ).apply { syncState() }
 
-        DrawerLayoutBackPressedDispatcher(binding.drawerLayout).bind(activity)
+        DrawerBackPressedDispatcher(binding.drawerLayout).bind(activity)
 
         val listeners = Listeners(activity)
         binding.navigationView.setNavigationItemSelectedListener(listeners)
@@ -92,29 +90,29 @@ class NavigationViewController(private val activity: AppCompatActivity) {
     @SuppressLint("RestrictedApi", "PrivateResource")
     private fun bindMenuIcons(context: Context, menu: Menu) {
         val parts = listOf(
-            R.id.menu_privacy_agreement to "\uea17",
-            R.id.menu_beta to "\ue859",
-            R.id.menu_star to "\ue838",
-            R.id.menu_email to "\ue0be",
+            R.id.menu_github to "\ue88a",           // home
+            R.id.menu_source to "\ue335",           // device_hub
+            R.id.menu_privacy_agreement to "\uea17",// policy
+            R.id.menu_beta to "\ue859",             // android
+            R.id.menu_star to "\ue838",             // star
+            R.id.menu_email to "\ue0be",            // email
         )
         var colorStateList: ColorStateList? = null
-        // com.google.android.material.R.styleable.NavigationView_itemIconTint
-        // default: com.google.android.material.R.color.m3_navigation_item_icon_tint
-        val typeValue = MaterialAttributes.resolve(
-            context,
-            com.google.android.material.R.attr.navigationViewStyle
-        )
+        // getColorStateList(R.styleable.NavigationView_itemIconTint)
+        // default: R.color.m3_navigation_item_icon_tint
+        val typeValue = MaterialAttributes.resolve(context, M3R.attr.navigationViewStyle)
         if (typeValue != null) {
             val typedArray = TintTypedArray.obtainStyledAttributes(
                 context,
                 typeValue.resourceId,
-                intArrayOf(com.google.android.material.R.attr.itemIconTint)
+                intArrayOf(M3R.attr.itemIconTint)
             )
             colorStateList = typedArray.getColorStateList(0)
             typedArray.recycle()
         }
         for (pair in parts) {
             menu.findItem(pair.first).icon = FontIconsDrawable(context, pair.second).apply {
+                setPadding(.5f.dp)
                 if (colorStateList != null) {
                     setColorStateList(colorStateList)
                 }
@@ -122,7 +120,7 @@ class NavigationViewController(private val activity: AppCompatActivity) {
         }
     }
 
-    private class DrawerLayoutBackPressedDispatcher(
+    private class DrawerBackPressedDispatcher(
         private val drawerLayout: DrawerLayout,
     ) : OnBackPressedCallback(false), Runnable, DrawerLayout.DrawerListener {
 
