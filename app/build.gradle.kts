f008af44444f91c064a8b36093b885e3f5d8c7e1
Hu Shenghao <dede.hu@qq.com>
2023-03-05 18:42:20 +0800

    chore: 更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.dede.android_eggs"
         minSdk = Versions.MIN_SDK
         targetSdk = Versions.TARGET_SDK
-        versionCode = 24
-        versionName = "1.9.1"
+        versionCode = 25
+        versionName = "1.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("zh", "en"))
