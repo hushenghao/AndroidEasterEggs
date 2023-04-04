55fe961d28d9cf5707b1be85ab74fead61ee339b
Hu Shenghao <dede.hu@qq.com>
2023-04-04 18:12:40 +0800

    feat: 添加开源协议入口

--- app/src/main/java/com/dede/android_eggs/main/holders/FooterHolder.kt
+++ app/src/main/java/com/dede/android_eggs/main/holders/FooterHolder.kt
@@ -6,8 +6,8 @@ import com.dede.android_eggs.R
 import com.dede.android_eggs.databinding.ItemEasterEggFooterBinding
 import com.dede.android_eggs.main.entity.Egg
 import com.dede.android_eggs.main.entity.Footer
-import com.dede.android_eggs.ui.adapter.VHolder
 import com.dede.android_eggs.ui.adapter.VHType
+import com.dede.android_eggs.ui.adapter.VHolder
 import com.dede.android_eggs.util.CustomTabsBrowser
 import com.dede.basic.string
 
@@ -20,7 +20,8 @@ class FooterHolder(view: View) : VHolder<Footer>(view), View.OnClickListener {
         binding.tvFrameworks.setOnClickListener(this)
         binding.tvStar.setOnClickListener(this)
         binding.tvBeta.setOnClickListener(this)
-        binding.tvPrivacyAgreement.setOnClickListener(this)
+        binding.tvPrivacy.setOnClickListener(this)
+        binding.tvLicense.setOnClickListener(this)
         binding.tvFeedback.setOnClickListener(this)
     }
 
@@ -35,21 +36,21 @@ class FooterHolder(view: View) : VHolder<Footer>(view), View.OnClickListener {
             }
             R.id.tv_star -> {
                 CustomTabsBrowser.launchUrlByBrowser(
-                    context, Uri.parse("market://details?id=%s".format(context.packageName))
+                    context,
+                    Uri.parse(context.getString(R.string.url_market_detail, context.packageName))
                 )
             }
             R.id.tv_beta -> {
                 CustomTabsBrowser.launchUrl(context, Uri.parse(R.string.url_beta.string))
             }
-            R.id.tv_privacy_agreement -> {
-                CustomTabsBrowser.launchUrl(
-                    context, Uri.parse(R.string.url_privacy_agreement.string)
-                )
+            R.id.tv_privacy -> {
+                CustomTabsBrowser.launchUrl(context, Uri.parse(R.string.url_privacy.string))
+            }
+            R.id.tv_license -> {
+                CustomTabsBrowser.launchUrl(context, Uri.parse(R.string.url_license.string))
             }
             R.id.tv_feedback -> {
-                CustomTabsBrowser.launchUrlByBrowser(
-                    context, Uri.parse("mailto:dede.hu@qq.com")
-                )
+                CustomTabsBrowser.launchUrlByBrowser(context, Uri.parse(R.string.url_mail.string))
             }
         }
     }
