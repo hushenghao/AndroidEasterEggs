35dabf07c04fc6c60f7706c3b2864fa26ad58a35
Hu Shenghao <dede.hu@qq.com>
2023-06-03 14:58:06 +0800

    feat: 添加返回手势动画

--- app/src/main/java/com/dede/android_eggs/main/BackPressedHandler.kt
+++ app/src/main/java/com/dede/android_eggs/main/BackPressedHandler.kt
@@ -1,30 +1,82 @@
 package com.dede.android_eggs.main
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.view.View
+import android.view.ViewConfiguration
 import android.window.BackEvent
 import androidx.activity.OnBackPressedCallback
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
+import androidx.core.content.withStyledAttributes
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import com.dede.android_eggs.R
+import com.dede.android_eggs.ui.drawables.ScaleType
+import com.dede.android_eggs.ui.drawables.ScaleTypeDrawable
 import com.dede.android_eggs.util.applyIf
+import com.dede.basic.dpf
+import com.dede.blurhash_android.BlurHashDrawable
+import com.google.android.material.carousel.MaskableFrameLayout
+import com.google.android.material.resources.MaterialAttributes
+import com.google.android.material.shape.ShapeAppearanceModel
+import kotlin.math.abs
+import kotlin.math.max
+import com.google.android.material.R as M3R
 
 class BackPressedHandler(private val host: AppCompatActivity) :
     OnBackPressedCallback(enabled = true), DefaultLifecycleObserver {
 
-    private val maxXShift = Resources.getSystem().displayMetrics.widthPixels / 20
-    private val content: View by lazy { host.findViewById(android.R.id.content) }
-    private var isStopped = false
+    companion object {
+        private const val ANIMATION_DELAY = 200L
+        private const val ANIMATION_DURATION = 200L
+    }
 
+    private val displayMetrics = Resources.getSystem().displayMetrics
+    private val maxXShift = displayMetrics.widthPixels / 20
+    private var maskableShapeSize: Float = 0f
+    private var scaledTouchSlop: Int = 0
+
+    private val contentView: MaskableFrameLayout by lazy { host.findViewById(R.id.fl_mask) }
+    private val androidContent: View by lazy { host.findViewById(android.R.id.content) }
+
+    private val backPressedDrawable: Drawable by lazy {
+        val blurHashDrawable = BlurHashDrawable(
+            host, R.string.hash_snapshot_bg,
+            (displayMetrics.widthPixels * 0.1f).toInt(),
+            (displayMetrics.heightPixels * 0.1f).toInt()
+        )
+        ScaleTypeDrawable(blurHashDrawable, ScaleType.CENTER_CROP)
+    }
+
+    private var isStopped = false
+    private var touchX = 0f
+    private var touchY = 0f
+    private var isProgressed = false
+
+    @SuppressLint("RestrictedApi")
     fun register() {
         host.onBackPressedDispatcher.addCallback(this)
         host.lifecycle.addObserver(this)
+
+        scaledTouchSlop = ViewConfiguration.get(host).scaledTouchSlop
+
+        val value = MaterialAttributes.resolveTypedValueOrThrow(
+            host, M3R.attr.shapeAppearanceCornerExtraLarge,
+            "undefined shapeAppearanceCornerExtraLarge"
+        )
+        val attrs = intArrayOf(M3R.attr.cornerSize)
+        host.withStyledAttributes(value.resourceId, attrs) {
+            maskableShapeSize = getDimension(0, 32.dpf)
+        }
     }
 
     override fun onStart(owner: LifecycleOwner) {
         if (!isStopped) return
-        isStopped = false
         // onRestart
         restoreContentState(true)
     }
@@ -35,32 +87,71 @@ class BackPressedHandler(private val host: AppCompatActivity) :
 
 
     private fun restoreContentState(delay: Boolean) {
-        content.animate()
+        contentView.animate()
             .scaleX(1f)
             .scaleY(1f)
             .translationX(0f)
             .translationY(0f)
-            .setDuration(300L)
+            .setDuration(ANIMATION_DURATION)
             .applyIf(delay) {
-                startDelay = 200L
+                startDelay = ANIMATION_DELAY
             }
             .start()
+        with(ObjectAnimator.ofFloat(maskableShapeSize, 0f)) {
+            addUpdateListener {
+                contentView.shapeAppearanceModel =
+                    createShapeAppearanceModel(it.animatedValue as Float)
+            }
+            doOnEnd {
+                androidContent.background = null
+            }
+            applyIf(delay) {
+                startDelay = ANIMATION_DELAY
+            }
+            duration = ANIMATION_DURATION
+            start()
+        }
+        isProgressed = false
+        isStopped = false
     }
 
+
+    @RequiresApi(34)
     override fun handleOnBackStarted(backEvent: BackEvent) {
+        touchX = backEvent.touchX
+        touchY = backEvent.touchY
     }
 
     @RequiresApi(34)
     override fun handleOnBackProgressed(backEvent: BackEvent) {
+        if (!isProgressed && max(
+                abs(touchX - backEvent.touchX),
+                abs(touchY - backEvent.touchY)
+            ) >= scaledTouchSlop
+        ) {
+            with(ObjectAnimator.ofFloat(0f, maskableShapeSize)) {
+                addUpdateListener {
+                    contentView.shapeAppearanceModel =
+                        createShapeAppearanceModel(it.animatedValue as Float)
+                }
+                doOnStart {
+                    androidContent.background = backPressedDrawable
+                }
+                duration = ANIMATION_DURATION
+                start()
+            }
+            isProgressed = true
+        }
+
         when (backEvent.swipeEdge) {
             BackEvent.EDGE_LEFT ->
-                content.translationX = backEvent.progress * maxXShift
+                contentView.translationX = backEvent.progress * maxXShift
 
             BackEvent.EDGE_RIGHT ->
-                content.translationX = -(backEvent.progress * maxXShift)
+                contentView.translationX = -(backEvent.progress * maxXShift)
         }
-        content.scaleX = 1F - (0.1F * backEvent.progress)
-        content.scaleY = 1F - (0.1F * backEvent.progress)
+        contentView.scaleX = 1F - (0.1F * backEvent.progress)
+        contentView.scaleY = 1F - (0.1F * backEvent.progress)
     }
 
     override fun handleOnBackCancelled() {
@@ -72,6 +163,10 @@ class BackPressedHandler(private val host: AppCompatActivity) :
         @Suppress("DEPRECATION")
         host.onBackPressed()
         isEnabled = true
+    }
+
+    private fun createShapeAppearanceModel(cornerSizes: Float): ShapeAppearanceModel {
+        return ShapeAppearanceModel.builder().setAllCornerSizes(cornerSizes).build()
     }
 
 }
