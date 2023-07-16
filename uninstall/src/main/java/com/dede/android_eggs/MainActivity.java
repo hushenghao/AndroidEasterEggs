1548743bfd5632b4afbc1f9ceeaea4886ab2cfe7
Hu Shenghao <dede.hu@qq.com>
2023-07-16 19:06:02 +0800

    feat: 优化卸载功能模块

--- uninstall/src/main/java/com/dede/android_eggs/MainActivity.java
+++ uninstall/src/main/java/com/dede/android_eggs/MainActivity.java
@@ -3,6 +3,7 @@ package com.dede.android_eggs;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,6 +16,8 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private static final String REASON_URL = "https://github.com/hushenghao/AndroidEasterEggs/blob/master/uninstall/README.md";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +27,18 @@ public class MainActivity extends Activity {
         SpannableStringBuilder message = new SpannableStringBuilder("This App is designed to solve:\n")
                 .append("Easter Eggs uninstall failed on some devices!\n",
                         new StyleSpan(Typeface.BOLD), Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
-                .append("Uninstall the app directly to uninstall Easter Eggs.");
+                .append("Uninstall this app to uninstall Easter Eggs.");
         textView.setText(message);
+
+        TextView tvReason = findViewById(R.id.tv_reason);
+        tvReason.setPaintFlags(tvReason.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    }
+
+    public void reason(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW)
+                .setData(Uri.parse(REASON_URL))
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     public void uninstall(View v) {
