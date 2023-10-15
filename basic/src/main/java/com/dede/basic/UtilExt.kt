c31af4afdeaf9d8c6bc2c50ab7a3b4a5209e02ba
Hu Shenghao <dede.hu@qq.com>
2023-10-15 13:07:23 +0800

    feat: Restore default Android T emoji group

--- basic/src/main/java/com/dede/basic/UtilExt.kt
+++ basic/src/main/java/com/dede/basic/UtilExt.kt
@@ -31,41 +31,6 @@ fun getEmojiUnicode(
     return list.joinToString(separator = separator, prefix = prefix, postfix = postfix)
 }
 
-/**
- * Unicode编码规则: Unicode码对每一个字符用4位16进制数表示。
- * 具体规则是：将一个字符(char)的高8位与低8位分别取出，转化为16进制数，
- * 如果转化的16进制数的长度不足2位，则在高位补0，然后将高、低8位转成的16进制字符串拼接起来并在前面补上"\\u" 即可。
- */
-fun String?.toUnicode(prefix: String = "\\u", join: String = ""): String {
-    if (this.isNullOrEmpty()) {
-        return ""
-    }
-    val sb = StringBuilder()
-    var s = true
-    var hex: Int
-    var hexStr: String
-    for (c in this.toCharArray()) {
-        if (s) {
-            s = false
-        } else {
-            sb.append(join)
-        }
-        // 加上\\u前缀
-        sb.append(prefix)
-        // 取出高8位
-        hex = c.code ushr 8
-        hexStr = Integer.toHexString(hex)
-        if (hexStr.length == 1) sb.append("0")
-        sb.append(hexStr)
-
-        // 取出低8位
-        hex = c.code and 0xFF
-        hexStr = Integer.toHexString(hex)
-        if (hexStr.length == 1) sb.append("0")
-        sb.append(hexStr)
-    }
-    return sb.toString()
-}
 
 val Number.dp: Int
     get() = TypedValue.applyDimension(
