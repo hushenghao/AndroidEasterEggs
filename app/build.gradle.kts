cce31de721c9123212136388f2988882e3e9af94
Hu Shenghao <dede.hu@qq.com>
2023-10-25 15:52:02 +0800

    doc: Update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dede.android_eggs"
         minSdk = Versions.MIN_SDK
         targetSdk = Versions.TARGET_SDK
-        versionCode = 32
-        versionName = "2.1.0"
+        versionCode = 33
+        versionName = "2.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resourceConfigurations += listOf(
