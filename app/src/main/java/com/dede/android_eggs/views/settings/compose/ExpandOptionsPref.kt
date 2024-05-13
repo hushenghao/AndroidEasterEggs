9288235b251cb2295547440ef74ac2ad8ae451a2
Hu Shenghao <dede.hu@qq.com>
2024-05-13 17:02:27 +0800

    fix: Settings group close animator
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- app/src/main/java/com/dede/android_eggs/views/settings/compose/ExpandOptionsPref.kt
+++ app/src/main/java/com/dede/android_eggs/views/settings/compose/ExpandOptionsPref.kt
@@ -2,7 +2,9 @@ package com.dede.android_eggs.views.settings.compose
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
@@ -73,7 +75,7 @@ fun ExpandOptionsPref(
         AnimatedVisibility(
             visible = expanded,
             enter = slideInVertically() + fadeIn(),
-            exit = shrinkVertically() + fadeOut(),
+            exit = shrinkVertically(animationSpec = spring(stiffness = Spring.StiffnessMedium)) + fadeOut(),
         ) {
             Column(
                 modifier = Modifier
