e1874513fbf03f7b1fbb2c89ce093dc97728e162
Hu Shenghao <dede.hu@qq.com>
2024-10-25 16:32:42 +0800

    chore: Disable :basic minify
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- build-logic/convention/src/main/kotlin/EasterEggBasicLibrary.kt
+++ build-logic/convention/src/main/kotlin/EasterEggBasicLibrary.kt
@@ -28,7 +28,7 @@ open class EasterEggBasicLibrary : Plugin<Project> {
 
                 buildTypes {
                     release {
-                        isMinifyEnabled = true
+                        isMinifyEnabled = false
                         proguardFiles(
                             getDefaultProguardFile("proguard-android-optimize.txt"),
                             "proguard-rules.pro"
