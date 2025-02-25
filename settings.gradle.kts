a26a0f194302145e759cc5a84096cdf3c0a038fd
Hu Shenghao <dede.hu@qq.com>
2025-02-25 16:40:30 +0800

    chore: Update google repository group regex
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,14 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         gradlePluginPortal()
-        google()
         mavenCentral()
     }
 }
