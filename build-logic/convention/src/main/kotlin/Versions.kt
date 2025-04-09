1d0b19add39c1f019842021029c2a09e9c502df1
Hu Shenghao <dede.hu@qq.com>
2025-04-09 11:21:46 +0800

    chore: Update Android 16 target version
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -2,9 +2,9 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 object Versions {
 
-    const val COMPILE_SDK = 35
-    const val BUILD_TOOLS = "35.0.0"
-    const val TARGET_SDK = 35
+    const val COMPILE_SDK = 36
+    const val BUILD_TOOLS = "36.0.0"
+    const val TARGET_SDK = 36
     const val MIN_SDK = 21
 
     val JAVA_VERSION: JavaLanguageVersion = JavaLanguageVersion.of(17)
