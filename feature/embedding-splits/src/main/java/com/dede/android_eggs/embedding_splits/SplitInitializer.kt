b2b6f8b32382ddab721411a195e2c3574a47ac66
Hu Shenghao <dede.hu@qq.com>
2025-04-30 10:24:42 +0800

    fix: Android 16 beta 4 window split crash
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- feature/embedding-splits/src/main/java/com/dede/android_eggs/embedding_splits/SplitInitializer.kt
+++ feature/embedding-splits/src/main/java/com/dede/android_eggs/embedding_splits/SplitInitializer.kt
@@ -2,9 +2,6 @@ package com.dede.android_eggs.embedding_splits
 
 import android.content.Context
 import androidx.startup.Initializer
-import androidx.window.WindowSdkExtensions
-import androidx.window.embedding.ActivityEmbeddingController
-import androidx.window.embedding.EmbeddingConfiguration
 import androidx.window.embedding.RuleController
 
 class SplitInitializer : Initializer<Unit> {
@@ -16,14 +13,15 @@ class SplitInitializer : Initializer<Unit> {
             ruleController.setRules(rules)
         } catch (ignore: RuntimeException) {
         }
-        if (WindowSdkExtensions.getInstance().extensionVersion >= 5) {
-            ActivityEmbeddingController.getInstance(context)
-                .setEmbeddingConfiguration(
-                    EmbeddingConfiguration.Builder()
-                        .setDimAreaBehavior(EmbeddingConfiguration.DimAreaBehavior.ON_TASK)
-                        .build()
-                )
-        }
+        // todo fix androidx.window:window:1.4.0-rc02, android 16 beta 4 crash
+//        if (WindowSdkExtensions.getInstance().extensionVersion >= 5) {
+//            ActivityEmbeddingController.getInstance(context)
+//                .setEmbeddingConfiguration(
+//                    EmbeddingConfiguration.Builder()
+//                        .setDimAreaBehavior(EmbeddingConfiguration.DimAreaBehavior.ON_TASK)
+//                        .build()
+//                )
+//        }
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
