32e12d4f85458cf57004ac5cb5e5b7f6ba0a9dff
Hu Shenghao <dede.hu@qq.com>
2025-06-05 18:47:39 +0800

    chore: Reset v3.5.0 beta number
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dede.android_eggs"
-        versionCode = 61
+        versionCode = 62
         versionName = "3.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -77,7 +77,7 @@ android {
         }
         create("beta") {
             dimension = "track"
-            versionNameSuffix = "-beta02"
+            versionNameSuffix = "-beta01"
         }
         create("product") {
             dimension = "track"
