05854720c5dfacb6f8f04810bd89f64b245f2e25
Hu Shenghao <dede.hu@qq.com>
2025-12-23 11:26:55 +0800

    revert: Easter Egg screen drawer width
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- app/src/main/java/com/dede/android_eggs/views/main/compose/EasterEggScreen.kt
+++ app/src/main/java/com/dede/android_eggs/views/main/compose/EasterEggScreen.kt
@@ -118,14 +118,15 @@ fun EasterEggScreen(
         val drawerState = rememberDrawerState(DrawerValue.Closed)
         ReverseModalNavigationDrawer(
             drawerContent = {
-                val maxWidth = LocalConfiguration.current.smallestScreenWidthDp * 0.8f
                 ModalDrawerSheet(
                     drawerState = drawerState,
                     drawerShape = shapes.extraLarge.end(0.dp),
                     windowInsets = WindowInsets(0, 0, 0, 0),
-                    modifier = Modifier.width(maxWidth.dp),
                 ) {
-                    SettingsScreen(drawerState)
+                    val maxWidth = LocalConfiguration.current.smallestScreenWidthDp * 0.8f
+                    Box(modifier = Modifier.width(maxWidth.dp)) {
+                        SettingsScreen(drawerState)
+                    }
                 }
             },
             drawerState = drawerState
