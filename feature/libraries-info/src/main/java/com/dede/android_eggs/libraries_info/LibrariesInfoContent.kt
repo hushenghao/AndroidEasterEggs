77dc7a0e9a7f9d0544e63c08022f0ce100a726a0
Hu Shenghao <dede.hu@qq.com>
2026-06-22 11:06:22 +0800

    build: Bump about-libraries from 14.2.1 to 15.0.0 #889
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- feature/libraries-info/src/main/java/com/dede/android_eggs/libraries_info/LibrariesInfoContent.kt
+++ feature/libraries-info/src/main/java/com/dede/android_eggs/libraries_info/LibrariesInfoContent.kt
@@ -2,30 +2,19 @@ package com.dede.android_eggs.libraries_info
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.platform.UriHandler
 import androidx.compose.ui.unit.dp
 import com.mikepenz.aboutlibraries.Libs
-import com.mikepenz.aboutlibraries.entity.Library
-import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
+import com.mikepenz.aboutlibraries.ui.compose.style.LibraryActionBadges
+import com.mikepenz.aboutlibraries.ui.compose.variant.LibraryActionKind
+import com.mikepenz.aboutlibraries.ui.compose.variant.LibraryActionMode
+import com.mikepenz.aboutlibraries.ui.compose.variant.LibraryBadges
+import com.mikepenz.aboutlibraries.ui.compose.variant.LibraryDetailMode
 import com.mikepenz.aboutlibraries.util.withContext
-
-internal fun Library.link(): String? =
-    (scm?.url ?: website ?: licenses.firstOrNull()?.url)?.replace("git://", "")
-
-internal fun Library.openLink(uriHandler: UriHandler) {
-    val link = this.link()
-    if (!link.isNullOrBlank()) {
-        uriHandler.openUri(link)
-    }
-}
 
 @Composable
 fun LibrariesInfoContent(
@@ -37,28 +26,34 @@ fun LibrariesInfoContent(
     val libraries = remember {
         Libs.Builder().withContext(context).build()
     }
-    var openLibrary: Library? by remember { mutableStateOf(null) }
     LibrariesContainer(
         libraries = libraries,
         modifier = modifier,
         contentPadding = contentPadding,
-        showDescription = true,
-        showFundingBadges = false,
-        dimensions = LibraryDefaults.libraryDimensions(),
-        onLibraryClick = {
-            val license = it.licenses.firstOrNull()
-            if (license == null || license.licenseContent.isNullOrBlank()) {
-                it.openLink(uriHandler)
-            } else {
-                openLibrary = it
+        badges = LibraryBadges(
+            author = true,
+            description = true,
+            version = true,
+            license = true,
+            funding = false,
+        ),
+        actionLabels = LibraryActionBadges(
+            sourceEnabled = true,
+            websiteEnabled = true,
+            licenseEnabled = true,
+            sponsorEnabled = false,
+        ),
+        detailMode = LibraryDetailMode.Sheet,
+        actionMode = LibraryActionMode.Icons,
+        onActionClick = { lib, kind ->
+            when (kind) {
+                LibraryActionKind.Source -> lib.scm?.url?.let { uriHandler.openUri(it) }
+                LibraryActionKind.Website -> lib.website?.let { uriHandler.openUri(it) }
+                LibraryActionKind.License ->
+                    lib.licenses.firstOrNull()?.url?.let { uriHandler.openUri(it) }
+                else -> return@LibrariesContainer false
             }
-        },
+            true
+        }
     )
-
-    if (openLibrary != null) {
-        LibrariesInfoDetail(
-            library = openLibrary!!,
-            onDismissRequest = { openLibrary = null },
-        )
-    }
 }
