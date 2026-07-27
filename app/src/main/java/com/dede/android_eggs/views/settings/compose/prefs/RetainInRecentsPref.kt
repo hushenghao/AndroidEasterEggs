ec484752c4f06e8c3206ac1e0960a61b00dfa4dc
Hu Shenghao <dede.hu@qq.com>
2026-07-27 14:40:13 +0800

    Revert "refactor: replace NEW_DOCUMENT launch with taskAffinity proxy activities for recents retention"
    This reverts commit 13552c9e4eebf74aba6d668081e6e1fbbbfb6b13.
    
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- app/src/main/java/com/dede/android_eggs/views/settings/compose/prefs/RetainInRecentsPref.kt
+++ app/src/main/java/com/dede/android_eggs/views/settings/compose/prefs/RetainInRecentsPref.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ViewCarousel
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.dede.android_eggs.util.pref
-import com.dede.android_eggs.views.main.util.EggActionHelp
 import com.dede.android_eggs.views.settings.compose.basic.SwitchPref
 import com.dede.android_eggs.views.settings.compose.basic.rememberPrefBoolState
 import com.dede.android_eggs.resources.R as StringsR
@@ -22,7 +20,6 @@ object RetainInRecentsPrefUtil {
 
 @Composable
 fun RetainInRecentsPref() {
-    val context = LocalContext.current
     SwitchPref(
         state = rememberPrefBoolState(
             RetainInRecentsPrefUtil.KEY_RETAIN_IN_RECENTS,
@@ -31,10 +28,5 @@ fun RetainInRecentsPref() {
         leadingIcon = Icons.Rounded.ViewCarousel,
         title = stringResource(id = StringsR.string.pref_retain_recent_egg_tasks),
         desc = stringResource(id = StringsR.string.pref_summary_retain_recent_egg_tasks),
-        onCheckedChange = { checked ->
-            if (!checked) {
-                EggActionHelp.cleanupProxyTasks(context)
-            }
-        }
     )
 }
