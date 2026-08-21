9518f108cca7fa5ce4746e1f92939418c859986b
Hu Shenghao <dede.hu@qq.com>
2026-08-21 09:35:41 +0800

    feat: Settings action button add animation
    Signed-off-by: Hu Shenghao <dede.hu@qq.com>

--- app/src/main/java/com/dede/android_eggs/views/main/compose/MainTitleBar.kt
+++ app/src/main/java/com/dede/android_eggs/views/main/compose/MainTitleBar.kt
@@ -10,12 +10,11 @@ import androidx.compose.animation.scaleOut
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBarDefaults.pinnedScrollBehavior
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
@@ -31,14 +30,12 @@ import com.dede.android_eggs.resources.R as StringsR
 @Preview
 fun MainTitleBar(
     modifier: Modifier = Modifier,
-    scrollBehavior: TopAppBarScrollBehavior = pinnedScrollBehavior(),
     searchBarState: BottomSearchBarState = rememberBottomSearchBarState(),
     drawerState: DrawerState? = null,
 ) {
     val scope = rememberCoroutineScope()
     HazeAppBar(
         modifier = modifier,
-        scrollBehavior = scrollBehavior,
         title = stringResource(R.string.app_name),
         navigationIcon = {
             AnimatedVisibility(
@@ -59,19 +56,26 @@ fun MainTitleBar(
             }
         },
         actions = {
-            IconButton(
-                onClick = {
-                    searchBarState.close()
-                    scope.launch {
-                        drawerState?.open()
-                    }
-                },
+            val drawerOpen = drawerState?.currentValue == DrawerValue.Open
+            AnimatedVisibility(
+                visible = !drawerOpen,
+                enter = fadeIn() + scaleIn(),
+                exit = fadeOut() + scaleOut(),
             ) {
-                Icon(
-                    imageVector = Icons.Rounded.SettingsOutline,
-                    contentDescription = stringResource(StringsR.string.label_settings),
-                    tint = MaterialTheme.colorScheme.onSurface,
-                )
+                IconButton(
+                    onClick = {
+                        searchBarState.close()
+                        scope.launch {
+                            drawerState?.open()
+                        }
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.SettingsOutline,
+                        contentDescription = stringResource(StringsR.string.label_settings),
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
             }
         },
     )
